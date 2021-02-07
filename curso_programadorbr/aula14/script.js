044907c1258855e2286a2481e8d19a470506d668
gilcemargz <gilcemargz@hotmail.com>
2021-02-07 09:37:09 -0300

    Fetch

--- curso_programadorbr/aula14/script.js
+++ curso_programadorbr/aula14/script.js
@@ -3,22 +3,22 @@ var x = 7;
 console.log(x);
 
 function speak() {
-    console.log(this.sound);
+  console.log(this.sound);
 }
 
 let dog = {
-    sound: 'au au',
-    speak: function () {
-        console.log(this.sound);
-    }   
-}
+  sound: "au au",
+  speak: function () {
+    console.log(this.sound);
+  },
+};
 
 let cat = {
-    sound: 'miau',
-    speak: function () {
-        console.log(this.sound);
-    }   
-}
+  sound: "miau",
+  speak: function () {
+    console.log(this.sound);
+  },
+};
 
 dog.speak();
 cat.speak();
@@ -27,42 +27,49 @@ let binded = speak.bind(dog);
 
 binded();
 
-function dobro(x){
-    return (x*2);
+function dobro(x) {
+  return x * 2;
 }
 
-let dobrar = function(x){
-    return (x*2);
-}
+let dobrar = function (x) {
+  return x * 2;
+};
 
-let metade = x => x / 2;
+let metade = (x) => x / 2;
 
-let expo = function (){
-    return this.x * this.x;
-}
+let expo = function () {
+  return this.x * this.x;
+};
 
 let numero = {
-    x: 25,
-    d: expo
-}
+  x: 25,
+  d: expo,
+};
 
-document.write(dobro(5)+'</br>');
-document.write(dobrar(8)+'</br>');
-document.write(metade(8)+'</br>');
-document.write(numero.d()+'</br>');
-
+document.write(dobro(5) + "</br>");
+document.write(dobrar(8) + "</br>");
+document.write(metade(8) + "</br>");
+document.write(numero.d() + "</br>");
 
 // callback
 
-
-function ola(){
-    return ('olá')
+function ola() {
+  return "olá";
 }
 
-
-
-function saudacao(s, nome){
-    return s() + " " + nome;
+function saudacao(s, nome) {
+  return s() + " " + nome;
 }
 
-document.write(saudacao(ola, "Gilcemar")+'</br>');
+document.write(saudacao(ola, "Gilcemar") + "</br>");
+
+let url = "https://api.exchangeratesapi.io/latest?baseUSD&symbols=BRL";
+
+fetch(url)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    //document.write(data);
+    console.log(data);
+  });
